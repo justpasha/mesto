9ae512b99f78c2d88a1eb9d245fa99823a40f6d7
Pavel Drobyshev <pavel.drob01@gmail.com>
2020-12-26 13:39:22 +0500

    fix: исправить замечания

--- scripts/script.js
+++ scripts/script.js
@@ -1,39 +1,34 @@
-let editButton = document.querySelector('.edit-button')
+let editButton = document.querySelector('.profile__edit-button')
 let popup = document.querySelector('.popup')
-let closeIcon=document.querySelector('.popup__close-icon')
+let closeButton=document.querySelector('.popup__close-button')
+let nameInput = popup.querySelector('.popup__input_type_name')
+let jobInput = popup.querySelector('.popup__input_type_about')
+let profileName = document.querySelector('.profile__name')
+let profileAbout = document.querySelector('.profile__description')
+let form = popup.querySelector('.popup__container')
 
-function toggleEditForm () {
+function changeEditForm () {
+  profileName.textContent = nameInput.value
+  profileAbout.textContent = jobInput.value
   popup.classList.toggle('popup_opened')
+}
+function closeEditForm () {
+  popup.classList.toggle('popup_opened')
+  nameInput.value = profileName.textContent
+  jobInput.value = profileAbout.textContent
+}
+function handleFormSubmit (evt) {
+    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
+    changeEditForm()
 }
 
 popup.addEventListener('click', (event) => {
   if (event.target === event.currentTarget) {
-    toggleEditForm();
+    closeEditForm();
   }
 });
-
-editButton.addEventListener('click',toggleEditForm)
-closeIcon.addEventListener('click',toggleEditForm)
-
-
-
-let form = popup.querySelector('.popup__container')
-
-function handleFormSubmit (evt) {
-    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
-    let nameInput = popup.querySelector('.popup__input_type_name')
-    let jobInput = popup.querySelector('.popup__input_type_about')
-    let profileName = document.querySelector('.profile__name')
-    let profileAbout = document.querySelector('.profile__description')
-
-    nameInput.getAttribute('value');
-    jobInput.getAttribute('value');
-
-    profileName.textContent = nameInput.value
-    profileAbout.textContent = jobInput.value
-    toggleEditForm()
-}
-
+editButton.addEventListener('click',changeEditForm)
+closeButton.addEventListener('click',closeEditForm)
 // Прикрепляем обработчик к форме:
 // он будет следить за событием “submit” - «отправка»
 form.addEventListener('submit', handleFormSubmit);
