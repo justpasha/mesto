2a9957972cb269dff5e0ebe290a59c902962dd4b
Pavel Drobyshev <pavel.drob01@gmail.com>
2021-03-27 01:13:35 +0500

    feat: улучшить ux

--- src/pages/index.js
+++ src/pages/index.js
@@ -77,7 +77,11 @@ const popupEditForm = new PopupWithForm(
   {
     popupSelector: '.edit-profile-form',
     handleFormSubmit: (formData) => {
-      api.editProfileInfo(formData).catch((err) => console.log(err));
+      changeLoading('.edit-profile-form', true, '');
+      api
+        .editProfileInfo(formData)
+        .catch((err) => console.log(err))
+        .finally(changeLoading('.edit-profile-form', false, 'Сохранить'));
       userInfo.setUserInfo(formData);
     },
   },
@@ -89,13 +93,15 @@ const popupAddForm = new PopupWithForm(
   {
     popupSelector: '.add-card-form',
     handleFormSubmit: (formData) => {
+      changeLoading('.add-card-form', true, '');
       api
         .createCard(formData)
         .then((res) => res.json())
         .then((newCardData) => {
           cardList.addItem(createLocalCard(newCardData));
         })
-        .catch((err) => console.log(err));
+        .catch((err) => console.log(err))
+        .finally(changeLoading('.add-card-form', false, 'Создать'));
     },
   },
   '.add-card-form__container'
@@ -106,13 +112,15 @@ const popupChangeAvatar = new PopupWithForm(
   {
     popupSelector: '.change-avatar-form',
     handleFormSubmit: (formData) => {
+      changeLoading('.change-avatar-form', true, '');
       api
         .changeAvatar(formData)
         .then((res) => res.json())
         .then((data) => {
           userInfo.setAvatar(data);
         })
-        .catch((err) => console.log(err));
+        .catch((err) => console.log(err))
+        .finally(changeLoading('.change-avatar-form', false, 'Сохранить'));
     },
   },
   '.change-avatar-form__container'
@@ -123,12 +131,14 @@ const popupDeleteForm = new PopupWithConfirm({
   popupSelector: '.delete-form',
   formSelector: '.delete-form__container',
   handleFormConfirm: (cardId, element) => {
+    changeLoading('.delete-form', true, '');
     api
       .deleteCard(cardId)
       .then(() => {
         element.remove();
       })
-      .catch((err) => console.log(err));
+      .catch((err) => console.log(err))
+      .finally(changeLoading('.delete-form', false, 'Да'));
   },
 });
 
@@ -152,27 +162,52 @@ function handleCardDelete(cardId, element) {
 
 //постановка лайка
 function setCardLike(cardId, likeActiveSelector, likeButton) {
+  likeLoading(likeButton, true);
   api
     .setLike(cardId)
     .then(() => {
       likeButton.classList.add(likeActiveSelector);
     })
-    .catch((err) => console.log(err));
+    .catch((err) => console.log(err))
+    .finally(likeLoading(likeButton, false));
 }
 
 //снятие лайка
 function deleteLike(cardId, likeActiveSelector, likeButton) {
+  likeLoading(likeButton, true);
   api
     .deleteLike(cardId)
     .then(() => {
       likeButton.classList.remove(likeActiveSelector);
     })
-    .catch((err) => console.log(err));
+    .catch((err) => console.log(err))
+    .finally(likeLoading(likeButton, false));
 }
 
 //открытие попапа с изображением
 function handleCardClick(image, name) {
   popupImage.open(image, name);
+}
+
+//обработка загрузок
+function changeLoading(popupSelector, isLoading, text) {
+  const popup = document.querySelector(popupSelector);
+  const formButton = popup.querySelector('.popup__save-button');
+  if (isLoading) {
+    formButton.textContent = 'Сохранение...';
+    formButton.setAttribute('disabled', true);
+  } else {
+    formButton.textContent = text;
+    formButton.removeAttribute('disabled');
+  }
+}
+
+function likeLoading(likeButton, isLoading) {
+  if (isLoading) {
+    likeButton.setAttribute('disabled', true);
+  } else {
+    likeButton.removeAttribute('disabled');
+  }
 }
 
 //изменить аватар
